04cae1708118f97ef9e90243faf492edf60af8a3
Alexander Sorokin <alex_grom@yahoo.com>
2017-12-13 21:16:51 +0300

    - Hopefully fixes node-red startup issues when yeelight is offline

--- yeelight/yeelight.js
+++ yeelight/yeelight.js
@@ -5,14 +5,42 @@ module.exports = function(RED) {
     function YeelightConnection(n) {
         RED.nodes.createNode(this,n);
         var node = this;
+
         if(this.credentials &&
             this.credentials.hostname && this.credentials.portnum){
 
-            this.light = Yeelight("yeelight://"+this.credentials.hostname+":"+this.credentials.portnum);
+
+            window.setTimeout(
+             (function(self) {
+                 return function() {
+                        self.setupConnection.apply(self, arguments);
+                    }
+             })(this), 1000
+            );
         }
+
         this.on('close', function() {
             this.light.exit();
         });
+
+        this.setupConnection = function(){
+            try {
+                this.light = Yeelight("yeelight://"+this.credentials.hostname+":"+this.credentials.portnum);
+            } catch(err) {
+                node.status({fill:"red",shape:"ring",text:err});
+                this.light = null;
+                this.error(err);
+
+                // try to reconnect in 5 minutes
+                window.setTimeout(
+                 (function(self) {
+                     return function() {
+                            self.setupConnection.apply(self, arguments);
+                        }
+                 })(this), 1000*60*5
+                );
+            }
+        }
 
     }
 
@@ -29,15 +57,9 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         this.config = RED.nodes.getNode(n.config);
         this.command = n.command
-        this.light = this.config ? this.config.light : null;
 
         var node = this;
 
-        if (!this.light) {
-			node.warn("Missing Yeelight config/Light not found");
-			node.status({fill:"red",shape:"ring",text:"Missing Yeelight Config/Light not found"});
-            return;
-        }
         node.status({});
 
         var msg = {};
@@ -46,6 +68,7 @@ module.exports = function(RED) {
         this.on('input', function (msg) {
 			try {
 				var cmd = this.command
+                this.light = this.config ? this.config.light : null;
 				this.light[cmd](msg.payload)
 			} catch(err) {
 				node.status({fill:"red",shape:"ring",text:err});
